44bf2972782afe3c9b4edccbe59dfbd916cebf19
Divy Deshmukh <127101632+DivyDeshmukh@users.noreply.github.com>
2024-02-07 05:09:51 +0000

    update

--- .devcontainer/practice.js
+++ .devcontainer/practice.js
@@ -1,11 +1,16 @@
-const arr = [1, 2, 3, 4, 5];
-const obj = {
-    name: 'Divy',
-    age: 21,
-    course: 'computer science'
+function constructorFunc (name, age) {
+    this.name = name;
+    this.age = age;
+    this.method = () => {
+        console.log('Running');
+    }
 }
 
-let result = arr.reduce ( (acc, initialVal) => {
-    return acc+initialVal;
-}, 0 );
-console.log(result);
+const userOne = new constructorFunc('Divy', 21);
+constructorFunc.hello = () => {
+    return ('Static Method');
+}
+console.log(constructorFunc.hello());
+// console.log(userOne.hello()); 
+// console.log(constructorFunc.method());
+console.log(typeof null);
